cec062aafe18a4394d244b255848e5bbaf089f13
t_ishikawa <t_ishikawa@zenrin-datacom.net>
2018-01-26 12:42:41 +0900

    add resources

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+ module.exports = merge(common, {
+   devtool: 'inline-source-map',
+   devServer: {
+     contentBase: path.resolve(__dirname),
+     host: 'localhost',
+     port: 55591,
+     watchOptions: {
+       watch: true
+     },
+     open: true
+   },
+});
